0ebff4642b46d24da2dd6205ba907ea8b127e6f3
Kenzo <swganzo@gmail.com>
2018-04-25 04:32:29 -0500

    ........

--- clientSide/src/app/@theme/theme.module.ts
+++ clientSide/src/app/@theme/theme.module.ts
@@ -1,7 +1,7 @@
 import { ModuleWithProviders, NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
-import { HttpClientModule, HttpClient } from '@angular/common/http';
+import { HttpClientModule } from '@angular/common/http';
 import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 
 import {
