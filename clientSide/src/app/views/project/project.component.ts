f86d7ec02ea1c7b42857ef92f3c65f53186d4743
Kenzo <swganzo@gmail.com>
2018-04-26 03:24:03 -0500

    .

--- clientSide/src/app/views/project/project.component.ts
+++ clientSide/src/app/views/project/project.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit, EventEmitter, Output } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { NgbModal } from '@ng-bootstrap/ng-bootstrap';
 import { ProjectModalComponent } from './project-modal/project-modal.component';
 import { SdUserService } from '../../@core/data/sdusers.service';
